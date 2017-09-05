0632fabcb4266ce5933787adf3ed9b742e1f9034
gravitejaa <grtrcr19@gmail.com>
2017-09-06 00:37:50 +0530

    Added HttpSession and redirecting login to userprofile page

--- src/App/LoginServlet.java
+++ src/App/LoginServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.RequestDispatcher;  
-
+import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class LoginServlet
  */
@@ -23,14 +23,17 @@ public class LoginServlet extends HttpServlet {
 		          
 		    String u=request.getParameter("username");  
 		    String p=request.getParameter("password");  
+		    
 		    if(LoginDao.validateUser(u)){
 		    
 		    	if(LoginDao.validate(u, p)){  
-		        RequestDispatcher rd=request.getRequestDispatcher("UserHomeServlet");  
-		        rd.forward(request,response);  
+		    		HttpSession session=request.getSession();
+		    		session.setAttribute("USER",u); 
+		    		RequestDispatcher rd=request.getRequestDispatcher("UserHomeServlet");  
+		    		rd.forward(request,response);  
 		    	}  
 		    	else{  
-		    		
+		    		out.write("<html><body><font color = red>Sorry username or password error </font></body></html>");
 		    		request.setAttribute("loginresponse", "Sorry username or password error" );
 		    		RequestDispatcher rd=request.getRequestDispatcher("login.html");  
 		    		rd.include(request,response);  
@@ -38,7 +41,7 @@ public class LoginServlet extends HttpServlet {
 		    }
 		    else
 		    {
-		    	out.println("Username not exists!, Register");
+		    	out.write("<html><body><font color = red>Username not exists!, Register </font></body></html>");
 		    	RequestDispatcher rd=request.getRequestDispatcher("login.html");  
 	    		rd.include(request,response); 
 		    }
--- src/App/UserHomeServlet.java
+++ src/App/UserHomeServlet.java
@@ -1,13 +1,15 @@
 package App;
 
 import java.io.IOException;
-import java.io.PrintWriter; 
+import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/UserHomeServlet")
 public class UserHomeServlet extends HttpServlet {
@@ -17,9 +19,17 @@ public class UserHomeServlet extends HttpServlet {
 		  
 		    response.setContentType("text/html");  
 		    PrintWriter out = response.getWriter();  
-		          
+		    HttpSession session=request.getSession(false);
+		    if(session==null){
+		    	out.print("Please login first");  
+	            request.getRequestDispatcher("login.html").include(request, response);
+		    }
+		    else{
+		    	RequestDispatcher rd=request.getRequestDispatcher("userprofile.html");
+				rd.include(request, response);
 		    String n=request.getParameter("username");  
-		    out.print("Welcome "+n);  
+		    out.print("Welcome "+n);
+		    }
 		          
 		    out.close();  
 		    }  
